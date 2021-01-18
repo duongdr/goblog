3c06d72f8f299ee123549466c559fbe81ef4f9c1
duong <duongdr>
2021-01-18 18:19:56 +0700

    db

---
+++ database/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `employees` (
+                            `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+                            `name` varchar(30) NOT NULL,
+                            `city` varchar(30) NOT NULL,
+                            PRIMARY KEY (`id`)
+)
